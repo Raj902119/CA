03cd83c6248bf161e015528d92db59fba18048e6
PawanSirsat <p1.sirsat1998@gmail.com>
2024-06-07 13:34:33 +0530

    Commit

--- src/component/ui/ImgSlider.jsx
+++ src/component/ui/ImgSlider.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import { Box, IconButton, Typography, Button } from '@mui/material';
 import SwipeableViews from 'react-swipeable-views';
@@ -140,3 +141,45 @@ const ImgSlider = () => {
 };
 
 export default ImgSlider;
+=======
+import React, { useState } from 'react'
+import SimpleImageSlider from 'react-simple-image-slider'
+import './ImgSlider.css' // Import the CSS file for styling
+
+const images = [
+  { url: 'assets/img5.jpeg', header: 'Team Building Workshop' },
+  { url: 'assets/img4.jpeg', header: 'Leadership Conference' },
+  { url: 'assets/img5.jpeg', header: 'Office Retreat' },
+  { url: 'assets/img4.jpeg', header: 'Employee Recognition Ceremony' },
+  { url: 'assets/img5.jpeg', header: 'Team Building Activity' },
+  { url: 'assets/img4.jpeg', header: 'Leadership Training' },
+]
+
+export const ImgSlider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const onSlideChange = (index) => {
+    if (index >= 0 && index < images.length) {
+      setCurrentIndex(index)
+    }
+  }
+
+  return (
+    <div className='img-slider-container'>
+      <SimpleImageSlider
+        width={'100%'}
+        height={750}
+        images={images}
+        showBullets={true}
+        showNavs={true}
+        onStartSlide={onSlideChange}
+      />
+      <div className='text-overlay'>
+        <h1>{images[currentIndex].header}</h1>
+      </div>
+    </div>
+  )
+}
+
+export default ImgSlider
+>>>>>>> Stashed changes
