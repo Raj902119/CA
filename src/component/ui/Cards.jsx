a04d3656a92d68bd6f7cab85dd445fa89c2a44ee
PawanSirsat <p1.sirsat1998@gmail.com>
2024-06-07 12:43:53 +0530

    Add new CSS

--- src/component/ui/Cards.jsx
+++ src/component/ui/Cards.jsx
@@ -17,11 +17,7 @@ const CardSlider = () => {
     },
     {
       url: 'https://www.youtube.com/embed/EIQOLf5KpYc?si=3mYvXOXus1dYUh0B',
-      title: 'YouTube video player 3',
-    },
-    {
-      url: 'https://www.youtube.com/embed/EIQOLf5KpYc?si=3mYvXOXus1dYUh0B',
-      title: 'YouTube video player 3',
+      title: 'YouTube video player 4',
     },
   ]
 
@@ -29,79 +25,57 @@ const CardSlider = () => {
   const totalSlides = videos.length
   const intervalRef = useRef(null)
   const autoScrollDelay = 3000
-  const autoScrollDirection = useRef(1) // 1: right, -1: left
-  const autoScrollCount = useRef(0)
 
   const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % totalSlides)
-  }
-
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + totalSlides) % totalSlides)
-  }
-
-  const handleAutoScroll = () => {
-    if (autoScrollCount.current === 2) {
-      autoScrollDirection.current = -1 // Change direction to left after 2 scrolls to right
-    } else if (autoScrollCount.current === 4) {
-      autoScrollDirection.current = 1 // Change direction to right after 2 scrolls to left
-      autoScrollCount.current = 0 // Reset count
-    }
-
-    if (autoScrollDirection.current === 1) {
-      nextSlide()
-    } else {
-      prevSlide()
-    }
-
-    autoScrollCount.current++
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % (totalSlides * 2))
   }
 
   useEffect(() => {
-    intervalRef.current = setInterval(handleAutoScroll, autoScrollDelay)
+    intervalRef.current = setInterval(nextSlide, autoScrollDelay)
 
     return () => {
       clearInterval(intervalRef.current)
     }
   }, [])
 
+  useEffect(() => {
+    if (currentIndex === totalSlides) {
+      setTimeout(() => {
+        setCurrentIndex(0)
+      }, 500) // Adjust this timeout to match the transition duration
+    }
+  }, [currentIndex, totalSlides])
+
   return (
-    <>
-      <div className='slide-container'>
-        <div
-          className='slide-content'
-          style={{
-            transform: `translateX(-${currentIndex * 40}%)`,
-            transition: 'transform 0.5s ease',
-          }}
-        >
-          {videos.map((video, index) => (
-            <div className='card' key={index}>
-              <div className='card-content-wrapper'>
-                <div className='image-content'>
-                  <iframe
-                    width='560'
-                    height='315'
-                    src={video.url}
-                    title={video.title}
-                    frameBorder='0'
-                    allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-                    referrerPolicy='strict-origin-when-cross-origin'
-                    allowFullScreen
-                  ></iframe>
-                </div>
+    <div className='slide-container'>
+      <div
+        className='slide-content'
+        style={{
+          transform: `translateX(-${(currentIndex % totalSlides) * 100}%)`,
+          transition:
+            currentIndex === totalSlides ? 'none' : 'transform 0.5s ease',
+        }}
+      >
+        {[...videos, ...videos].map((video, index) => (
+          <div className='card' key={index}>
+            <div className='card-content-wrapper'>
+              <div className='image-content'>
+                <iframe
+                  width='560'
+                  height='315'
+                  src={video.url}
+                  title={video.title}
+                  frameBorder='0'
+                  allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+                  referrerPolicy='strict-origin-when-cross-origin'
+                  allowFullScreen
+                ></iframe>
               </div>
             </div>
-          ))}
-        </div>
-        <div className='navBtn left' onClick={prevSlide}>
-          &lt;
-        </div>
-        <div className='navBtn right' onClick={nextSlide}>
-          &gt;
-        </div>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   )
 }
 
