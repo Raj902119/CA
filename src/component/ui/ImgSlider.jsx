c30f5acad99ee7f9c51e8f2f7b2ebcb9722dcfb7
PawanSirsat <p1.sirsat1998@gmail.com>
2024-06-08 11:09:11 +0530

    Commit

--- src/component/ui/ImgSlider.jsx
+++ src/component/ui/ImgSlider.jsx
@@ -1,18 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import SimpleImageSlider from 'react-simple-image-slider'
 import './ImgSlider.css' // Import the CSS file for styling
 
 const images = [
-  { url: 'assets/img5.jpeg', header: 'Team Building Workshop' },
-  { url: 'assets/img4.jpeg', header: 'Leadership Conference' },
-  { url: 'assets/img5.jpeg', header: 'Office Retreat' },
-  { url: 'assets/img4.jpeg', header: 'Employee Recognition Ceremony' },
-  { url: 'assets/img5.jpeg', header: 'Team Building Activity' },
-  { url: 'assets/img4.jpeg', header: 'Leadership Training' },
+  {
+    url: 'assets/img5.jpeg',
+    header: 'Team Building Workshop',
+    text: 'Team Building Workshop',
+  },
+  {
+    url: 'assets/img4.jpeg',
+    header: 'Leadership Conference',
+    text: 'Team Building Workshop',
+  },
+  {
+    url: 'assets/img5.jpeg',
+    header: 'Office Retreat',
+    text: 'Team Building Workshop',
+  },
+  {
+    url: 'assets/img4.jpeg',
+    header: 'Employee Recognition Ceremony',
+    text: 'Team Building Workshop',
+  },
+  {
+    url: 'assets/img5.jpeg',
+    header: 'Team Building Activity',
+    text: 'Team Building Workshop',
+  },
+  {
+    url: 'assets/img4.jpeg',
+    header: 'Leadership Training',
+    text: 'Team Building Workshop',
+  },
 ]
 
 export const ImgSlider = () => {
   const [currentIndex, setCurrentIndex] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
+    }, 3200) // Change slide every 5 seconds
+
+    return () => clearInterval(interval) // Cleanup interval on component unmount
+  }, [])
 
   const onSlideChange = (index) => {
     if (index >= 0 && index < images.length) {
@@ -28,10 +60,19 @@ export const ImgSlider = () => {
         images={images}
         showBullets={true}
         showNavs={true}
+        startIndex={currentIndex}
         onStartSlide={onSlideChange}
       />
-      <div className='text-overlay'>
-        <h1>{images[currentIndex].header}</h1>
+      <div className='text-overlay drop-shadow-lg'>
+        <h1 className='drop-shadow-lg'>{images[currentIndex].header}</h1>
+        <div className={`transition-opacity duration-500 opacity-100`}>
+          <p className='transition-opacity duration-500 drop-shadow-lg'>
+            {images[currentIndex].text}
+          </p>
+          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg mt-4 transition-opacity duration-500'>
+            Read More
+          </button>
+        </div>
       </div>
     </div>
   )
